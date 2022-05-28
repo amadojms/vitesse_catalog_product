56e397a41296c5a63cbff542757155208b92010a
Amado Martin Solis <amadojms@gmail.com>
2022-05-27 19:06:40 -0500

    first commit

---
+++ src/components/CardProduct.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { Product } from '~/models/Product';
+const props = defineProps<{
+  product: Product
+}>()
+</script>
+
+<template>
+  <div pt-10 bg-blend-color mt-5 bg-green-500 hover:bg-green-700 border-radius-10>
+    <v-card>{{product.name}}</v-card>
+    
+  </div>
+</template>
---
+++ src/components/Products.vue
@@ -0,0 +1,20 @@
+
+<script setup lang="ts">
+import type { Product } from '~/models/Product';
+// import type { CardProduct } from '~/components/CardProduct.vue';
+import products_json from '~/assets/products.json';
+
+const props = defineProps<{
+  products?: Product,
+}>()
+
+// const { count, inc, dec } = useCounter(props.initial)
+</script>
+
+<template>
+  <div class="">
+    <template v-for="(p, i) in products_json" :key="i">
+      <card-product :product="p"></card-product>
+    </template>
+  </div>
+</template>
